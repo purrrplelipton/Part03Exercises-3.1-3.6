a4ad4214d557f92b0f6d47871152fb2bb90dc055
purrrplelipton <immanueltoby@gmail.com>
2022-05-16 15:40:38 +0100

    update: final solution to part 3 ex. 3.1 - 3.6

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const express = require("express")
+const express = require("express");
+
 const app = express()
 
-app.use(express.json())
+app.use(express.json());
+
+const morgan = require("morgan");
+
+const port = process.env.PORT || 3013
 
 let persons = [
     { 
@@ -26,34 +31,31 @@ let persons = [
     }
 ]
 
-app.get("/", (req, res) => {
-  return (
-    res.send(
-      `<h1>phonebook with express</h1>`
-    )
-  )
+const errorHandler = (err, req, res, next) => {
+  if(err) {
+    res.json({ err: err });
+  }
+};
+
+app.get("/", (req, res, next) => {
+  res.send(`<h1>phonebook application with express</h1>`)
 })
 
 app.get("/api/persons", (req, res) => {
-  return (
-    res.json(persons)
-  )
+  res.json(persons)
 })
 
 app.get("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
-  return (
-    person ? res.json(person) : res.status(404).end()
-  )
+  person ? res.json(person) : res.status(404).end()
 })
 
 app.get("/info", (req, res) => {
-  let prsnsLen = persons.length
-  return res.send(`
-    <p>Phonebook has info for ${prsnsLen} people</p>
-    <p>${new Date()}</p>
-  `)
+  res.send(
+    `<h1>Phonebook has info for ${persons.length} people</h1>
+    <h3>${new Date()}</h3>`
+  )
 })
 
 const rngId = () => {
@@ -63,28 +65,23 @@ const rngId = () => {
   }
 }
 
-app.post("/api/persons", (req, res) => {
+morgan.token(`body`, (req) => JSON.stringify(req.body))
+
+app.post("/api/persons", morgan(`:method :url :status :res[content-length] - :response-time ms :body`), (req, res) => {
+  const regExp = new RegExp(req.body.name, "i")
   const contact = req.body
   const content = { ...contact, id: rngId() }
 
-  if(
-      !req.body.hasOwnProperty("name") ||
-      !req.body.hasOwnProperty("number") ||
-      !Boolean(req.body.name.trim()) ||
-      req.body.number.replace(/\s/g, "").length > 13 ||
-      req.body.number.replace(/\s/g, "").length < 6
-    ) {
+  if( !req.body.hasOwnProperty("name") || !req.body.hasOwnProperty("number") || !Boolean(req.body.name.trim()) || req.body.number.replace(/\s/g, "").length > 13 || req.body.number.replace(/\s/g, "").length < 6 ) {
     console.log(`invalid name or number`)
+    res.send(`<h1>invalid name or number</h1>`)
     res.status(400).end()
-  } else if(
-    persons.findIndex(person => (
-      person.name.toLowerCase() === content.name.toLowerCase()
-    )) !== -1
+  } else if(persons.findIndex(person => person.name.match(regExp)) !== -1
     ) {
     console.log(`${req.body.name} already exists`)
     res.status(400).end()
   } else {
-    persons.concat(content)
+    persons = persons.concat(content)
     console.log(content)
     res.json(content)
   }
@@ -94,10 +91,11 @@ app.post("/api/persons", (req, res) => {
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id === id)
-  return res.status(204).end()
+  res.status(204).end()
 })
 
-const port = 3013
+app.use(errorHandler);
+
 app.listen(port, () => (
   console.log(`server running on port ${port}`)
-))
+));
